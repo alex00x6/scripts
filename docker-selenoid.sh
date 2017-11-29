d848912b4c469741a3d001579820991a583cd78c
Alexander <alex00x6@users.noreply.github.com>
2017-11-29 18:51:28 +0200

    Update docker-selenoid.sh

--- docker-selenoid.sh
+++ docker-selenoid.sh
@@ -1,3 +1,2 @@
-docker run --rm -it -v /var/run/docker.sock:/var/run/docker.sock -v ${HOME}:/root -e OVERRIDE_HOME=${HOME} aerokube/cm:latest-release selenoid start --vnc --tmpfs 128 --last-versions 2 --browsers chrome,firefox --args "-limit 10 -timeout 1200s" --force
+docker run --rm -it -v /var/run/docker.sock:/var/run/docker.sock -v ${HOME}:/root -e OVERRIDE_HOME=${HOME} aerokube/cm:latest-release selenoid start --vnc --tmpfs 128 --last-versions 2 --browsers chrome,firefox --args "-limit 10 -timeout 1200s -conf /etc/selenoid/browsers.json" --force
 docker run --rm -it -v /var/run/docker.sock:/var/run/docker.sock -v ${HOME}:/root -e OVERRIDE_HOME=${HOME} aerokube/cm:latest-release selenoid-ui start
-
